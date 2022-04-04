9b3b8ac680d0c72fcb67bbd8551ad44d36f44020
Cristian Le <git@lecris.me>
2022-04-04 23:26:04 +0900

    Improve Sstate settings
    Signed-off-by: Cristian Le <git@lecris.me>

--- .teamcity/asteroid/AsteroidProject.kt
+++ .teamcity/asteroid/AsteroidProject.kt
@@ -9,73 +9,73 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.activeStorag
 import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.versionedSettings
 
 object AsteroidProject : Project({
-    description = "Base Asteroid Project"
+	description = "Base Asteroid Project"
 
-    // Attach vcsRoots from CoreVCS
-    for (vcs in CoreVCS.all())
-        if (vcs != CoreVCS.MetaSmartwatch)
-            vcsRoot(vcs)
+	// Attach vcsRoots from CoreVCS
+	for (vcs in CoreVCS.all())
+		if (vcs != CoreVCS.MetaSmartwatch)
+			vcsRoot(vcs)
 
-    // Attach InitWorkspace build
-    buildType(InitWorkspace)
+	// Attach InitWorkspace build
+	buildType(InitWorkspace)
 
-    // Attach the subProjects
-    // TODO: Link to subprojects externally
-    subProjects(
-        asteroid.devices.DevicesProject,
-        asteroid.packages.PackagesProject,
-        asteroid.thirdparty.ThirdPartyProject
-    )
+	// Attach the subProjects
+	// TODO: Link to subprojects externally
+	subProjects(
+		asteroid.devices.DevicesProject,
+		asteroid.packages.PackagesProject,
+		asteroid.thirdparty.ThirdPartyProject
+	)
 
-    // Define the build parameters inherited by the subprojects
-    params {
-        text(
-            "system.sstate.server.location",
-            "/var/www/asteroidos",
-            label = "Sstate server location",
-            description = "Path location of the sstate-cache",
-            readOnly = true, allowEmpty = true
-        )
-        text(
-            "system.sstate.server.user",
-            "asteroidos",
-            label = "SState server user",
-            description = "Username used to upload sstate-cache",
-            readOnly = true,
-            allowEmpty = true
-        )
-        text(
-            "system.sstate.server.upload_address",
-            "192.168.0.2",
-            label = "SState server backend address",
-            description = "Backend address to upload the sstate-cache",
-            readOnly = true,
-            allowEmpty = false
-        )
-        text(
-            "system.sstate.server.address",
-            "https://asteroidos.lecris.dev",
-            label = "SState server public address",
-            description = "Public address serving sstate-cache",
-            readOnly = true,
-            allowEmpty = false
-        )
-    }
+	// Define the build parameters inherited by the subprojects
+	params {
+		text(
+			"system.sstate.server.address",
+			Settings.sstateServer.url,
+			label = "SState server public address",
+			description = "Public address serving sstate-cache",
+			readOnly = true,
+			allowEmpty = false
+		)
+		if (Settings.deploySstate) {
+			text(
+				"system.sstate.server.location",
+				Settings.sstateServer.location ?: "",
+				label = "Sstate server location",
+				description = "Path location of the sstate-cache",
+				readOnly = true, allowEmpty = true
+			)
+			text(
+				"system.sstate.server.user",
+				Settings.sstateServer.user ?: "",
+				label = "SState server user",
+				description = "Username used to upload sstate-cache",
+				readOnly = true, allowEmpty = true
+			)
+			text(
+				"system.sstate.server.upload_address",
+				Settings.sstateServer.backendUrl ?: "",
+				label = "SState server backend address",
+				description = "Backend address to upload the sstate-cache",
+				readOnly = true, allowEmpty = false
+			)
+		}
+	}
 
-    features {
-        activeStorage {
-            activeStorageID = "DefaultStorage"
-        }
-    }
-}){
-    override fun validate(consumer: ErrorConsumer) {
-        super.validate(consumer)
-        if (Settings.deploySstate){
-            // TODO: check that SSH key is present
-            //  Currently this feature is unavailable
-        }
-        if (Settings.pullRequests || Settings.commitStatus){
-            // TODO: check that SSH key and and OAuth token are present
-        }
-    }
+	features {
+		activeStorage {
+			activeStorageID = "DefaultStorage"
+		}
+	}
+}) {
+	override fun validate(consumer: ErrorConsumer) {
+		super.validate(consumer)
+		if (Settings.deploySstate) {
+			// TODO: check that SSH key is present
+			//  Currently this feature is unavailable
+		}
+		if (Settings.pullRequests || Settings.commitStatus) {
+			// TODO: check that SSH key and and OAuth token are present
+		}
+	}
 }
--- .teamcity/asteroid/Settings.kt
+++ .teamcity/asteroid/Settings.kt
@@ -3,25 +3,40 @@ package asteroid
 import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
 
 object Settings {
-    val asteroidPackages = DslContext.getParameter("Packages")
-        .split("[\\s,]".toRegex()).toList()
-        .filterNot { it.isEmpty() }
-//    val communityPackages = DslContext.getParameter("CommunityPackages")
+	val asteroidPackages = DslContext.getParameter("Packages")
+		.split("[\\s,]".toRegex()).toList()
+		.filterNot { it.isEmpty() }
+
+	//    val communityPackages = DslContext.getParameter("CommunityPackages")
 //        .split("[\\s,]".toRegex()).toList()
 //        .filterNot { it.isEmpty() }
-    val devices = DslContext.getParameter("Devices")
-        .split("[\\s,]".toRegex()).toList()
-        .filterNot { it.isEmpty() }
+	val devices = DslContext.getParameter("Devices")
+		.split("[\\s,]".toRegex()).toList()
+		.filterNot { it.isEmpty() }
 
-    val fork = DslContext.getParameter("Fork")
-    val upstream = DslContext.getParameter("Upstream","AsteroidOS")
-    val deploySstate = DslContext.getParameter("DeploySstate", "false").toBoolean()
+	val fork = DslContext.getParameter("Fork")
+	val upstream = DslContext.getParameter("Upstream", "AsteroidOS")
+	val deploySstate = DslContext.getParameter("DeploySstate", "false").toBoolean()
 
-    // TODO: Change to Github app when available
-    val commitStatus = DslContext.getParameter("CommitStatus", "false").toBoolean()
-    val commitUser = DslContext.getParameter("CommitUser", fork)
-    val pullRequests = DslContext.getParameter("PullRequests", "false").toBoolean()
-    init {
-        // TODO: Check validity of Token and Ssh key
-    }
+	object sstateServer {
+		val url = DslContext.getParameter("SstateServerURL", "https://sstate.asteroid.org")
+		val backendUrl = if (deploySstate)
+			DslContext.getParameter("SstateServerBackendURL", "sstate.asteroid.org")
+		else null
+		val user = if (deploySstate)
+			DslContext.getParameter("SstateServerUser", "asteroidos")
+		else null
+		val location = if (deploySstate)
+			DslContext.getParameter("SstateServerLocation", "")
+		else null
+	}
+
+	// TODO: Change to Github app when available
+	val commitStatus = DslContext.getParameter("CommitStatus", "false").toBoolean()
+	val commitUser = DslContext.getParameter("CommitUser", fork)
+	val pullRequests = DslContext.getParameter("PullRequests", "false").toBoolean()
+
+	init {
+		// TODO: Check validity of Token and Ssh key
+	}
 }
